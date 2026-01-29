6f595e21dc5f5ec3f4519e62aae9e3a82acdd8e2
Javiergzm <Javier.guzman@stu.vertexacademies.org>
2026-01-29 19:10:58 +0000

    Changed the username,password and 2FA question

--- multi_factor_JG.py
+++ multi_factor_JG.py
@@ -4,9 +4,13 @@ import multifactorcli as mfc
 # create a multi-factor interface to a restricted app
 my_auth = mfc.MultiFactorAuth()
 
+username = "jgdabest"
+password = "01bowhueywhuey"
+my_auth.set_authetication(username,password)
+
 # set the users authentication information
-question = "What is your favorite color?"
-answer = "purple"
+question = "What is the name of the first shoes you got?"
+answer = "sketchers"
 my_auth.set_multiFactorAuthentication(question, answer)
 
 my_auth.run()
